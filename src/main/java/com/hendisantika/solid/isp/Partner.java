7cb8a4535a94302267573322fb13caa0e89f9d53
Hendi Santika <hendisantika@gmail.com>
2019-08-07 06:05:21 +0700

    #16 - Add Partner Impl in Inversion Segregation Principle

---
+++ src/main/java/com/hendisantika/solid/isp/Partner.java
@@ -0,0 +1,16 @@
+package com.hendisantika.solid.isp;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : solid-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-08-07
+ * Time: 06:04
+ */
+public interface Partner extends Employee {
+    BigDecimal getProfits();
+}
